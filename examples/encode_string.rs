644cb31ef8144e1020ac90c3b4270c2955115973
Riccardo Casatta <riccardo@casatta.it>
2023-09-13 15:43:06 +0200

    Encode example, optionally take data from shell param

--- examples/encode_string.rs
+++ examples/encode_string.rs
@@ -1,4 +1,7 @@
 fn main() {
-    let qr_code = qr_code::QrCode::new(b"Hello").unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    let default = "Hello".to_string();
+    let data = args.get(1).unwrap_or(&default);
+    let qr_code = qr_code::QrCode::new(data.as_bytes()).unwrap();
     println!("{}", qr_code.to_string(true, 3));
 }
